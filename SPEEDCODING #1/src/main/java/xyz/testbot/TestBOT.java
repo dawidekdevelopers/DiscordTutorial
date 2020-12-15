746cd278cdb47dffd112d91d0f2afd44cbc80189
dawidekdevelopers <63783028+dawidekdevelopers@users.noreply.github.com>
2020-12-15 22:23:30 +0100

    Update TestBOT.java

--- SPEEDCODING #1/src/main/java/xyz/testbot/TestBOT.java
+++ SPEEDCODING #1/src/main/java/xyz/testbot/TestBOT.java
@@ -15,7 +15,7 @@ public class TestBOT {
 
     public static void start() {
         try {
-            JDABuilder.create("Nzg4NDg1MDgxNDAzODgzNTYw.X9kL9A.F2b-LDClDk9A0b34Jd2n4isMnAQ", GatewayIntent.GUILD_EMOJIS, GatewayIntent.DIRECT_MESSAGE_REACTIONS, GatewayIntent.DIRECT_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_BANS, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGE_TYPING, GatewayIntent.GUILD_PRESENCES)
+            JDABuilder.create("BOT_TOKEN", GatewayIntent.GUILD_EMOJIS, GatewayIntent.DIRECT_MESSAGE_REACTIONS, GatewayIntent.DIRECT_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_BANS, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGE_TYPING, GatewayIntent.GUILD_PRESENCES)
                     .setStatus(OnlineStatus.DO_NOT_DISTURB)
                     .setActivity(Activity.playing("Fajny bocik :D"))
                     .build();
